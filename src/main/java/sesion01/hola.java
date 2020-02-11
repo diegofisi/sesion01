d1501027bf2624a37fcbe3e58f9c9c45a3143b2f
diegofisi <diego704249@gmail.com>
2020-02-10 21:14:29 -0500

    Primer commit

---
+++ src/main/java/sesion01/hola.java
@@ -0,0 +1,20 @@
+package sesion01;
+
+import java.util.Scanner;
+
+public class hola {
+
+	public static void main(String[] args) {
+		// imprime los mensaje
+		System.out.println("Bienvenido al curso");
+		//captura los valores
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("¿cual es tu nombre?");
+
+		String nombre = sc.nextLine();
+		System.out.println("bienvenido " + nombre);
+
+	}
+
+}
