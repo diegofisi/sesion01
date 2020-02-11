5a66f1be881f3d3a54afe88bc1d373e15f48ddfe
diegofisi <diego704249@gmail.com>
2020-02-10 21:40:36 -0500

    segundo commit

--- src/main/java/sesion01/hola.java
+++ src/main/java/sesion01/hola.java
@@ -6,7 +6,7 @@ public class hola {
 
 	public static void main(String[] args) {
 		// imprime los mensaje
-		System.out.println("Bienvenido al curso");
+		System.out.println("Bienvenido al curso 2021");
 		//captura los valores
 		Scanner sc = new Scanner(System.in);
 
